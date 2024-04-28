2faa06344b268119affba3acc9b254e7d269c032
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2024-04-28 12:47:34 +0300

    Snap to surface to prevent from flying off from slope surfaces

--- Assets/Source/Scripts/Movement.cs
+++ Assets/Source/Scripts/Movement.cs
@@ -43,24 +43,27 @@ namespace PlotvaIzLodzya.Player.Movement.CollideAndSlide
             _collideDepth = 5;
         }
 
-
-        // Высокая скорость позволяте слетать с наклонных поврехностей
         private void Update()
         {
             var vel = _input.Direction * _speed;
-
 
             if (_applyGravity)
                 vel += _wordlConfig.Gravity;
 
             vel += _exteranalForce;
+
+            IsGrounded = _collisionHandler.IsCollide(transform.position, -_characterConfig.Up, out RaycastHit hit, 2 * _clipPreventingValue);
+
+            if (IsGrounded)
+                vel = SnapVelocityToSurface(vel, hit.normal);
+
             Move(vel);
 
             if (Input.GetKeyDown(KeyCode.Space))
                 Jump();
 
-            IsGrounded = _collisionHandler.IsCollide(transform.position, -_characterConfig.Up, out RaycastHit hit, 2 * _clipPreventingValue);
         }
+
         public void Jump()
         {
             Debug.Log($"Can jump: {IsGrounded}");
@@ -68,8 +71,15 @@ namespace PlotvaIzLodzya.Player.Movement.CollideAndSlide
 
         public void Move(Vector3 vel)
         {
-            vel = CollideAndSlide_recursive(vel * Time.deltaTime, _transform.position, 0);
+            vel = CollideAndSlide_recursive(vel * Time.deltaTime, _transform.position);
             _transform.position += vel;
+        }
+
+        private Vector3 SnapVelocityToSurface(Vector3 vel, Vector3 normal) 
+        {
+            vel.y = 0;
+            vel = ProjectOnSurface(vel, normal);
+            return vel;
         }
 
         private Vector3 ProjectOnSurface(Vector3 vector, Vector3 normal)
@@ -79,14 +89,14 @@ namespace PlotvaIzLodzya.Player.Movement.CollideAndSlide
             return vector;
         }
 
-
-        private Vector3 CollideAndSlide_recursive(Vector3 vel, Vector3 currentPos, int depth)
+        private Vector3 CollideAndSlide_recursive(Vector3 vel, Vector3 currentPos, int currentDepth = 0)
         {
-            if (depth >= _collideDepth)
+            if (currentDepth >= _collideDepth)
                 return Vector3.zero;
 
             float dist = vel.magnitude + _clipPreventingValue;
             var dir = vel.normalized;
+
             RaycastHit hit;
 
             if (_collisionHandler.IsCollide(currentPos, dir, out hit, dist))
@@ -97,12 +107,12 @@ namespace PlotvaIzLodzya.Player.Movement.CollideAndSlide
                 var nextPos = currentPos + velToNextStep;
 
                 float angle = Vector3.Angle(_wordlConfig.WorldUp, hit.normal);
-                //Debug.Log(angle);
+
                 var projectedleftOverVel = ProjectOnSurface(leftOverVel, hit.normal).normalized * leftOverVel.magnitude;
 
                 projectedleftOverVel = HandleSlope(angle, vel, projectedleftOverVel, hit.normal);
 
-                vel = velToNextStep + CollideAndSlide_recursive(projectedleftOverVel, nextPos, depth + 1);
+                vel = velToNextStep + CollideAndSlide_recursive(projectedleftOverVel, nextPos, currentDepth + 1);
 
                 return vel;
             }
@@ -122,7 +132,6 @@ namespace PlotvaIzLodzya.Player.Movement.CollideAndSlide
 
             if (IsTooSteepSlope(slopeAngle))
             {
-                projectedleftOverVel.y = vel.y;
                 projectedleftOverVel = ScaleByHorizontalVelocity(vel, projectedleftOverVel, surfaceNormal);
             }
             else
@@ -131,12 +140,12 @@ namespace PlotvaIzLodzya.Player.Movement.CollideAndSlide
                 projectedleftOverVel = vel.normalized * projectedleftOverVel.magnitude;
             }
 
-
             return projectedleftOverVel;
         }
 
         public Vector3 ScaleByHorizontalVelocity(Vector3 vel, Vector3 projectedVel, Vector3 surfaceNormal)
         {
+            projectedVel.y = vel.y;
             vel.y = 0;
             surfaceNormal.y = 0;
             float scale = 1 + Vector3.Dot(vel.normalized, surfaceNormal.normalized);
