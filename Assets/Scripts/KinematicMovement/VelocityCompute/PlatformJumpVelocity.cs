6b9572c7dffcd7123a8e9329f60fb8ac7f357115
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2025-01-24 14:20:09 +0300

    platform jump veloctity bug fix

--- Assets/Scripts/KinematicMovement/VelocityCompute/PlatformJumpVelocity.cs
+++ Assets/Scripts/KinematicMovement/VelocityCompute/PlatformJumpVelocity.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 namespace PlotvaIzLodzya.KinematicMovement.VelocityCompute
 {
     [CreateAssetMenu(fileName = nameof(PlatformJumpVelocity), menuName = "SO/" + nameof(VelocityComputation) + "/" + nameof(PlatformJumpVelocity), order = 2)]
-    public class PlatformJumpVelocity : KeepMomentumAirborneVelocity
+    public class PlatformJumpVelocity : VelocityComputation
     {
         private IPlatformProvider _platformProvider;
-
+        private AirborneVelocityCompute _airborneVelocity;
         public override bool CanTransit => State.IsOnPlatform && State.IsJumping;
 
-        public void Init(IPlatformProvider platformProvider, IMovementState state, MovementConfig movementConfig)
+        public override Vector3 Velocity
         {
-            base.Init(state, movementConfig);
+            get
+            {
+                return _airborneVelocity.Velocity;
+            }
+            set
+            {
+                _airborneVelocity.Velocity = value;
+            }
+        }
+
+        public void Init(IMovementState state, IPlatformProvider platformProvider, AirborneVelocityCompute airborneVelocity)
+        {
+            State = state;
+            _airborneVelocity = airborneVelocity;
             _platformProvider = platformProvider;
         }
 
         public override void Enter(Vector3 currentVelocity)
         {
-            var platformVelocity = _platformProvider.Platform.UnscaledVelocity;
-            AddMaxHorSpeed(platformVelocity.magnitude);
-            base.Enter(currentVelocity + platformVelocity);
+            var platformVelocity = Vector3.zero;
+
+            //Some wierd bug when you touch platform after jumping off it
+            if(_platformProvider.Platform != null)
+                platformVelocity = _platformProvider.Platform.UnscaledVelocity;
+
+            _airborneVelocity.AddMaxHorSpeed(platformVelocity.magnitude);
+            _airborneVelocity.Enter(currentVelocity + platformVelocity);
         }
+
+        public override Vector3 CalculateHorizontalSpeed(Vector3 dir, float deltaTime)=> _airborneVelocity.CalculateHorizontalSpeed(dir, deltaTime);
+        public override float CalculateVerticalSpeed(float deltaTime) => _airborneVelocity.CalculateVerticalSpeed(deltaTime);
+        public override void Exit() => _airborneVelocity.Exit();
+        public override void Jump(float speed) => _airborneVelocity.Jump(speed);
+        public override void SetVelocity(Vector3 velocity) => _airborneVelocity.SetVelocity(velocity);
     }
 }
